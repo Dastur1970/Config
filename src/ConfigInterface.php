ad0f80552ae8a32b3f2a3063e72cd30ef824a04c
Dastur1970 <dastur1970@gmail.com>
2017-06-18 18:11:24 -0700

    Initial Commit

---
+++ src/ConfigInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * PHP version 5.6
+ *
+ * @category Config
+ * @package  Config
+ * @author   Dastur1970 <dastur1970@gmail.com>
+ * @license  https://github.com/Dastur1970/Config/blob/master/LICENSE
+ *           MIT License
+ * @link     https://github.com/Dastur1970/Config
+ */
+
+namespace Dastur\Config;
+
+/**
+ * The configuration class interface.
+ *
+ * @category Config
+ * @package  Config
+ * @author   Dastur1970 <dastur1970@gmail.com>
+ * @license  https://github.com/Dastur1970/Config/blob/master/LICENSE
+ *           MIT License
+ * @link     https://github.com/Dastur1970/Config/
+ */
+interface ConfigInterface
+{
+
+}
